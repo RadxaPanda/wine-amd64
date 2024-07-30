842c307ff3077b3224d353d3a0354773dafec5a1
RadxaPanda <152165783+RadxaPanda@users.noreply.github.com>
2024-07-30 23:27:36 +0000

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-
+set -x
 
 script_dir=$(dirname "$(realpath "$0")")
 version=$(dpkg-parsechangelog --show-field Version)
 url="https://github.com/Kron4ek/Wine-Builds/releases/download/${version}/wine-${version}-amd64.tar.xz"
 
+rm -rf "${script_dir}/wine/" "${script_dir}/"wine-*-amd64.tar.xz* "${script_dir}"/wine-*-amd64/
 wget "$url"
-tar -xf "wine-${version}-amd64.tar.xz"
-mv wine-${version}-amd64/ wine/
-mkdir -p wine/usr/
-mv wine/{bin,lib,share} wine/usr/
+tar -xf "${script_dir}/wine-${version}-amd64.tar.xz"
+mv -v "${script_dir}/wine-${version}-amd64/" "${script_dir}/wine/"
+mkdir -vp "${script_dir}/wine/usr/"
+mv -v ${script_dir}/wine/{bin,lib,share} ${script_dir}/wine/usr/
